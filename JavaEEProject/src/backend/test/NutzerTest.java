f90086bad2a664c52e0e9cf605fd7611683259e2
Markus <markus.giesbrecht@live.com>
2018-03-23 17:42:54 +0100

    Add Project to IntelliJ Added Maven depends. Fix Maven junit problem

--- JavaEEProject/src/backend/test/NutzerTest.java
+++ JavaEEProject/src/backend/test/NutzerTest.java
@@ -11,13 +11,13 @@ public class NutzerTest {
 	private NutzerHandler myBean = new NutzerHandler();
 	@Test
 	public void testCreateNutzer() {
-		assertEquals(ErrorHandler.NUTZERSCHONVORHANDEN, myBean.createNutzer("Halil", "Özdogan", "halil", "start123", "Manager"));
+		assertEquals(ErrorHandler.NUTZERSCHONVORHANDEN, myBean.createNutzer("Halil", "Oezdogan", "halil", "start123", "Manager"));
 		assertEquals(ErrorHandler.NUTZERSCHONVORHANDEN, myBean.createNutzer("Dagobert", "Duck", "dagobert", "start123", "Mitarbeiter"));
-		assertEquals(ErrorHandler.NUTZERDATENUNVOLLSTAENDIG, myBean.createNutzer("", "Özdogan", "user", "start123", "Manager"));
+		assertEquals(ErrorHandler.NUTZERDATENUNVOLLSTAENDIG, myBean.createNutzer("", "Oezdogan", "user", "start123", "Manager"));
 		assertEquals(ErrorHandler.NUTZERDATENUNVOLLSTAENDIG, myBean.createNutzer("Halil", "", "user", "start123", "Manager"));
-		assertEquals(ErrorHandler.NUTZERDATENUNVOLLSTAENDIG, myBean.createNutzer("Halil", "Özdogan", "", "start123", "Manager"));
-		assertEquals(ErrorHandler.NUTZERDATENUNVOLLSTAENDIG, myBean.createNutzer("Halil", "Özdogan", "user", "", "Manager"));
-		assertEquals(ErrorHandler.NUTZERDATENUNVOLLSTAENDIG, myBean.createNutzer("Halil", "Özdogan", "user", "start123", ""));
+		assertEquals(ErrorHandler.NUTZERDATENUNVOLLSTAENDIG, myBean.createNutzer("Halil", "Oezdogan", "", "start123", "Manager"));
+		assertEquals(ErrorHandler.NUTZERDATENUNVOLLSTAENDIG, myBean.createNutzer("Halil", "Oezdogan", "user", "", "Manager"));
+		assertEquals(ErrorHandler.NUTZERDATENUNVOLLSTAENDIG, myBean.createNutzer("Halil", "Oezdogan", "user", "start123", ""));
 	}
 
 }
