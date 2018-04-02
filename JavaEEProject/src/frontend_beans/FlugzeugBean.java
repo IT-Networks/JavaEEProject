e32c636c6c3682f820047026330a5fdee84fb4f5
Markus <markus.giesbrecht@live.com>
2018-04-02 19:10:23 +0200

    Add some Functions

---
+++ JavaEEProject/src/frontend_beans/FlugzeugBean.java
@@ -0,0 +1,64 @@
+package frontend_beans;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import backend.enterpriseLogic.FlugzeugHandler;
+import backend.enterpriseLogic.SuccessHandler;
+
+
+@ManagedBean
+@RequestScoped
+public class FlugzeugBean { 
+
+	@NotNull
+	private String procducer;
+	@NotNull
+	private String typ;
+	@NotNull
+	@Min(10)
+	private int seats;	
+	
+	
+	public String getProcducer() {
+		return procducer;
+	}
+	public void setProcducer(String procducer) {
+		this.procducer = procducer;
+	}
+	public String getTyp() {
+		return typ;
+	}
+	public void setTyp(String typ) {
+		this.typ = typ;
+	}
+	public int getSeats() {
+		return seats;
+	}
+	public void setSeats(int seats) {
+		this.seats = seats;
+	}
+	
+	public void createFlugzeug(){
+		FacesMessage msg;
+		FlugzeugHandler fh = new FlugzeugHandler();
+		String result = fh.createFlugzeug(procducer, typ, seats);
+		
+		if(result.equals(SuccessHandler.FLUGZEUGANLAGE)) {
+			msg = new FacesMessage(FacesMessage.SEVERITY_INFO,result,"");
+			FacesContext.getCurrentInstance().addMessage("airplainForm:saveAirplain", msg);
+		}
+		else {
+			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,result,"");
+			FacesContext.getCurrentInstance().addMessage("airplainForm:saveAirplain", msg);
+		}
+	}
+
+	
+
+	
+}
---
+++ JavaEEProject/src/frontend_beans/PassengerBean.java
@@ -0,0 +1,125 @@
+package frontend_beans;
+
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import backend.enterpriseLogic.FlugHandler;
+import backend.enterpriseLogic.FlugzeugHandler;
+import backend.enterpriseLogic.MahlzeitHandler;
+import backend.enterpriseLogic.PassagierHandler;
+import backend.enterpriseLogic.SuccessHandler;
+
+
+@ManagedBean
+@RequestScoped
+public class PassengerBean { 
+
+	@NotNull
+	private String name;
+	@NotNull
+	private String lastname;
+	@NotNull
+	private String location;
+	@NotNull
+	private String birthdy;
+	@NotNull
+	private String nationality;
+
+	
+
+	public String getName() {
+		return name;
+	}
+
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+
+	public String getLastname() {
+		return lastname;
+	}
+
+
+
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+
+
+
+	public String getLocation() {
+		return location;
+	}
+
+
+
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+
+
+
+	public String getBirthdy() {
+		return birthdy;
+	}
+
+
+
+
+	public void setBirthdy(String birthdy) {
+		this.birthdy = birthdy;
+	}
+
+
+
+
+	public String getNationality() {
+		return nationality;
+	}
+
+
+
+
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+
+
+
+	public void creatPessenger(){
+		FacesMessage msg;
+		PassagierHandler ph = new PassagierHandler();
+		String result = ph.createPassagier(name, lastname, location, birthdy, nationality);
+		
+		if(result.equals(SuccessHandler.MAHLZEITANLAGE)) {
+			msg = new FacesMessage(FacesMessage.SEVERITY_INFO,result,"");
+			FacesContext.getCurrentInstance().addMessage("passengerForm:creatPessenger", msg);
+		}
+		else {
+			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,result,"");
+			FacesContext.getCurrentInstance().addMessage("passengerForm:creatPessenger", msg);
+		}
+	}
+	
+
+	
+
+	
+}
